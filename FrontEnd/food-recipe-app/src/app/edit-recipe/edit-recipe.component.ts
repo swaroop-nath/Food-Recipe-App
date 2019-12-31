4bc5b64c516363bf5a7586c8686aff79756b26f7
swaroop-nath <swaroopnath6@gmail.com>
2019-12-31 10:21:40 +0530

    Done partly

--- FrontEnd/food-recipe-app/src/app/edit-recipe/edit-recipe.component.ts
+++ FrontEnd/food-recipe-app/src/app/edit-recipe/edit-recipe.component.ts
@@ -15,17 +15,41 @@ export class EditRecipeComponent implements OnInit {
   dish: Dish;
 
   categoryList: string[] = [];
-  ingredientList: string[] = [];
+  ingredientGrid: Ingredient[][] = [];
+  selectedIngredientFlag: Map<Ingredient, Boolean>;
 
-  selectedCategory: any;
-  selectedIngredients: any;
+  selectedCategory: any[];
 
   constructor(private service: FoodRecipeService) { 
     // this.dish = service.transitDish;
     this.dish = new Dish();
 
     service.loadAllCategories().forEach(category => this.categoryList.push(category.categoryName));
-    service.loadAllIngredients().forEach(ingredient => this.ingredientList.push(ingredient.ingredientName));
+    this.makeGridOfIngredients(service.loadAllIngredients());
+    
+    this.selectedIngredientFlag = new Map<Ingredient, Boolean>();
+  }
+
+  makeGridOfIngredients(loadedIngredients: Ingredient[]): void {
+    let index = 0;
+    let limit = loadedIngredients.length;
+
+    while (index < limit) {
+      let indicator = 0;
+      let tempArray = [];
+      
+      while (indicator < 4 && index < limit) {
+        tempArray.push(loadedIngredients[index]);
+        index += 1;
+
+        let randomNumber = Math.floor(Math.random() * 4);
+        
+        if (randomNumber == 0)
+          break;
+      }
+
+      this.ingredientGrid.push(tempArray);
+    }
   }
 
   ngOnInit() {
@@ -37,6 +61,15 @@ export class EditRecipeComponent implements OnInit {
         $('.file-upload-image').removeClass('deep-blur');
         $('.file-upload-image-plus').removeClass('increase-opacity');
     });
+
+    $('td').click(function() {
+      console.log('hello');
+      
+    })
+  }
+  
+  clickOnIngredient(row: number, col: number): void {
+    this.selectedIngredientFlag.set(this.ingredientGrid[row][col], !this.selectedIngredientFlag.get(this.ingredientGrid[row][col]));
   }
 
   uploadDish(): void {
@@ -45,7 +78,7 @@ export class EditRecipeComponent implements OnInit {
 
     if (this.dish.category === undefined)
       this.dish.category = new Category(0, selectedCategoryName);
-      
+
     
   }
 
